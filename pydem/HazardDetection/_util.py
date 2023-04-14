8a3675ea0eabad152554c5d1791a77dfaa6336ee
Kento <kentomita634@gmail.com>
2023-04-14 15:18:34 -0400

    pixel locations of landing pads

--- pydem/HazardDetection/_util.py
+++ pydem/HazardDetection/_util.py
@@ -4,7 +4,7 @@ import numpy as np
 from numba import jit, prange
 
 
-def max_under_pad(n_rpad:int, dem:np.ndarray):
+def max_under_pad(n_rpad:int, dem:np.ndarray) -> np.ndarray:
     """Return max values under landing pad for each pixel. 
     Args:
         n_rpad: number of pixels to represent radius of landing pad
@@ -20,6 +20,7 @@ def max_under_pad(n_rpad:int, dem:np.ndarray):
     fpmap = _max_under_pad(dem.shape[0], dem.shape[1], n_rpad, dem, fpmap)
 
     return fpmap + zmin
+
 
 @jit(nopython=True, fastmath=True, nogil=True, cache=True, parallel=True)
 def _max_under_pad(h:int, w:int, n_rpad:int, dem:np.ndarray, fpmap:np.ndarray):
@@ -46,3 +47,99 @@ def _max_under_pad(h:int, w:int, n_rpad:int, dem:np.ndarray, fpmap:np.ndarray):
                 
                 fpmap[xi, yi] = val
     return fpmap
+
+
+
+def pad_pix_locations(theta_arr:np.ndarray, lander_type:str, rmpp: float, N_RLANDER:int, N_RPAD:int):
+    """Return the list of relative locations (pix) of landing pads
+    Args:
+        theta_arr: (n,), angle of landing orientation
+        lander_type: "triangle" or "square"
+        rmpp: resolution (m/pix)
+        N_RLANDER: number of pixels to represent radius of lander
+        N_RPAD: number of pixels to represent radius of landing pad
+    """
+    # + ---------> y (axis 1)
+    # |
+    # |
+    # v
+    # x (axis 0)
+    # Theta: counter clockwise
+
+    assert lander_type == "triangle" or lander_type == "square"
+
+    n = theta_arr.size
+
+    xi_arr = np.zeros(shape=(n, 4)).astype(np.int32)
+    yi_arr = np.zeros(shape=(n, 4)).astype(np.int32)
+
+    if lander_type=="triangle":
+        xi_arr, yi_arr, = _pad_locs_tri(n, theta_arr, xi_arr, yi_arr, N_RLANDER, N_RPAD)
+    else:  # lander_type=="square":
+        xi_arr, yi_arr, = _pad_locs_sq(n, theta_arr, xi_arr, yi_arr, N_RLANDER, N_RPAD)
+
+    x_arr = (xi_arr * rmpp).astype(np.float32)
+    y_arr = (yi_arr * rmpp).astype(np.float32)
+
+    return xi_arr, yi_arr, x_arr, y_arr
+
+
+@jit(nopython=True, fastmath=True, nogil=True, cache=True, parallel=True)
+def _pad_locs_tri(n:int, th_arr:np.ndarray, xi_arr:np.ndarray, yi_arr:np.ndarray, N_RLANDER:int, N_RPAD:int):
+    """Return the list of relative locations (pix) of landing pads; triangle lander"""
+    # + ---------> y (axis 1)
+    # |
+    # |
+    # v
+    # x (axis 0)
+    # Theta: counter clockwise
+
+    R = N_RLANDER - N_RPAD
+    for i in prange(n):
+        th = th_arr[i]
+        n_rcos = int(R * np.cos(th))
+        n_rsin = int(R * np.sin(th))
+
+        # pad 0: left pad
+        # pad 1: bottom right pad
+        # pad 2: top right pad
+        xi_0, yi_0 = int(R * np.sin(th)), int(R * np.cos(th + np.pi))
+        xi_1, yi_1 = int(R * np.sin(th + np.pi/3)), int(R * np.cos(th + np.pi * 4/3))
+        xi_2, yi_2 = int(R * np.sin(th - np.pi/3)), int(R * np.cos(th + np.pi * 2/3))
+        xi_3, yi_3 = 0, 0  # dummy
+
+        xi_arr[i] = np.array([xi_0, xi_1, xi_2, xi_3]).astype(np.int32)
+        yi_arr[i] = np.array([yi_0, yi_1, yi_2, yi_3]).astype(np.int32)
+
+    return xi_arr, yi_arr
+
+
+@jit(nopython=True, fastmath=True, nogil=True, cache=True, parallel=True)
+def _pad_locs_sq(n:int, th_arr:np.ndarray, xi_arr:np.ndarray, yi_arr:np.ndarray, N_RLANDER:int, N_RPAD:int):
+    """Return the list of relative locations (pix) of landing pads; square lander"""
+    # + ---------> y (axis 1)
+    # |
+    # |
+    # v
+    # x (axis 0)
+    # Theta: counter clockwise
+
+    R = N_RLANDER - N_RPAD
+    for i in prange(n):
+        th = th_arr[i]
+        n_rcos = int(R * np.cos(th))
+        n_rsin = int(R * np.sin(th))
+
+        # pad 0: left pad
+        # pad 1: bottom pad
+        # pad 2: top pad
+        # pad 3: right pad
+        xi_0, yi_0 =  n_rsin, -n_rcos
+        xi_1, yi_1 =  n_rcos,  n_rsin
+        xi_2, yi_2 = -n_rcos, -n_rsin
+        xi_3, yi_3 = -n_rsin,  n_rcos
+
+        xi_arr[i] = np.array([xi_0, xi_1, xi_2, xi_3]).astype(np.int32)
+        yi_arr[i] = np.array([yi_0, yi_1, yi_2, yi_3]).astype(np.int32)
+
+    return xi_arr, yi_arr
