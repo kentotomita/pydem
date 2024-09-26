ef4fc8ab37609354613d8cae9931eb49d6554250
Tomo <tomohiro.sasaki@gatech.edu>
2024-09-26 14:51:38 -0400

    Dem_generation (#2)
    * Create data generation script

--- generate_dataset.py
+++ generate_dataset.py
@@ -14,57 +14,100 @@ import pydem.Geometry as gm
 import pydem.Graphic as pyvis
 import pydem.HazardDetection as hd
 import pydem.util as util
-
-
-# generate a rocky terrain as an example
-nh, nw = 512, 512 # height and width of the terrain
-res = 0.1 # resolution of the terrain
-
-# generate a rocky terrain
-dem = st.rocky_terrain(shape=(nh, nw), res=res, k=0.3, dmax=1.5, dmin=0.1) # k=0.3 means 30% of the terrain is covered by rocks
-
-# generate a crater? #FIXME: what is this?
-terrain = st.dsa(dem, hmax=0.7) 
-
-# smooth the terrain
 from scipy.ndimage import gaussian_filter
-dem += gaussian_filter(terrain, sigma=2)
+
+nh, nw = 512, 512 # height and width of the dem
+res = 0.1        # resolution of the dem
+
+dl = 3.0
+dp = 0.3
+rmpp = 0.1
+
+# Genearate random dems and save both the dem and site_safe labels
+n_training = 2000   # number of training samples
+n_validation = 200  # number of validation samples
+n_test = 200        # number of test samples
+
+# create training dem directory
+import os
+os.makedirs('./data/training/depth_maps', exist_ok=True)
+os.makedirs('./data/training/label/is_safe', exist_ok=True)
+os.makedirs('./data/validation/depth_maps', exist_ok=True)
+os.makedirs('./data/validation/label/is_safe', exist_ok=True)
+os.makedirs('./data/test/depth_maps', exist_ok=True)
+os.makedirs('./data/test/label/is_safe', exist_ok=True)
+
+for i in range(n_training):
+    dem = st.rocky_terrain(shape=(nh, nw), res=res, k=0.1, dmax=1.5, dmin=0.1)
+    terrain = st.dsa(dem, hmax=0.7)
+
+    
+    dem_train = dem + gaussian_filter(terrain, sigma=2)
+    depth_data = dem_train.copy()
+    x, y = np.mgrid[0:depth_data.shape[0], 0:depth_data.shape[1]]
+    fpmap, site_slope, site_rghns, pix_rghns, site_safe, indef = hd.dhd(
+        dem_train, 
+        rmpp=rmpp, 
+        negative_rghns_unsafe=False,
+        lander_type='square', 
+        dl=dl, 
+        dp=dp,
+        scrit=10*np.pi/180,
+        rcrit=0.3)
+    
+    # save 8digit file name
+    dem_file = f'./data/training/depth_maps/{i:08d}.npy'
+    site_safe_file = f'./data/training/label/is_safe/{i:08d}.npy'
+    np.save(dem_file, dem_train)
+    np.save(site_safe_file, site_safe)
+
+    # log 
+    print(f"Training: {i}/{n_training}")
+    
+    if i <= n_validation:
+        dem_valid = dem + gaussian_filter(terrain, sigma=2)
+        depth_data = dem_valid.copy()
+        x, y = np.mgrid[0:depth_data.shape[0], 0:depth_data.shape[1]]
+        fpmap, site_slope, site_rghns, pix_rghns, site_safe, indef = hd.dhd(
+            dem_valid, 
+            rmpp=rmpp, 
+            negative_rghns_unsafe=False,
+            lander_type='square', 
+            dl=dl, 
+            dp=dp,
+            scrit=10*np.pi/180,
+            rcrit=0.3)
+        
+        # save 8digit file name
+        dem_file = f'./data/validation/depth_maps/{i:08d}.npy'
+        site_safe_file = f'./data/validation/label/is_safe/{i:08d}.npy'
+        np.save(dem_file, dem_valid)
+        np.save(site_safe_file, site_safe)
+    
+        print(f"Validation: {i}/{n_validation}")
+
+    if i <= n_test:
+        dem_test = dem + gaussian_filter(terrain, sigma=2)
+        depth_data = dem_test.copy()
+        x, y = np.mgrid[0:depth_data.shape[0], 0:depth_data.shape[1]]
+        fpmap, site_slope, site_rghns, pix_rghns, site_safe, indef = hd.dhd(
+            dem_test, 
+            rmpp=rmpp, 
+            negative_rghns_unsafe=False,
+            lander_type='square', 
+            dl=dl, 
+            dp=dp,
+            scrit=10*np.pi/180,
+            rcrit=0.3)
+        
+        # save 8digit file name
+        dem_file = f'./data/test/depth_maps/{i:08d}.npy'
+        site_safe_file = f'./data/test/label/is_safe/{i:08d}.npy'
+        np.save(dem_file, dem_test)
+        np.save(site_safe_file, site_safe)
+
+        print(f"Test: {i}/{n_test}")
 
 
-dl = 3.0 #FIXME: what is this?
-dp = 0.3 #FIXME: what is this?
-rmpp = 0.1 #FIXME: what is this?
+print("Done!")
 
-
-fpmap, site_slope, site_rghns, pix_rghns, site_safe, indef = hd.dhd(
-    dem, 
-    rmpp=rmpp, 
-    negative_rghns_unsafe=False,
-    lander_type='square', 
-    dl=dl, 
-    dp=dp,
-    scrit=10*np.pi/180,
-    rcrit=0.3
-)
-
-# Create mesh from DEM
-h, w = dem.shape
-x = np.linspace(0, nh * res, h)
-y = np.linspace(0, nw * res, w)
-xc, yc = np.mean(x), np.mean(y)
-x -= xc
-y -= yc
-yy, xx = np.meshgrid(y, x)
-mesh = pv.StructuredGrid(xx, yy, dem)
-
-# Add scalar attributes to mesh
-mesh.point_data.set_array(fpmap.flatten('F'), 'fpmap')
-mesh.point_data.set_array(site_slope.flatten('F'), 'site_slope')
-mesh.point_data.set_array(site_rghns.flatten('F'), 'site_rghns')
-mesh.point_data.set_array(pix_rghns.flatten('F'), 'pix_rghns')
-mesh.point_data.set_array(site_safe.flatten('F'), 'site_safe')
-
-mesh.set_active_scalars('site_safe')
-# mesh.plot()
-plt.imshow(site_safe, cmap='gray')
-plt.show()
